65ac3354248256af99128c2c8c09c76a773901bd
Tvisher <tvisher@mail.ru>
2022-11-18 14:08:56 +0600

    ETC

---
+++ docs/js/app.js
@@ -0,0 +1,26 @@
+
+async function testWebP() {
+    const hasWebP = await new Promise(res => {
+        const webP = new Image();
+        webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+        webP.onload = webP.onerror = () => {
+            res(webP.height === 2);
+        };
+    });
+    let className = hasWebP === true ? 'webp' : 'no-webp';
+    document.documentElement.classList.add(className);
+};
+
+// Проверка поддержки webP
+testWebP();
+
+
+
+$('.marquee').marquee({
+    startVisible: true,
+    duration: 30000,
+    // gap: 100,
+    delayBeforeStart: 0,
+    direction: 'left',
+    duplicated: true,
+});
